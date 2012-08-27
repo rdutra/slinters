82cccad59822a6febeec8a9958fb08b507013c60
rdutra <rdutra@timbasoftware.com>
2012-08-27 14:20:42 -0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Slinters::Application.config.secret_token = '91d3a2561e0be2a56921c85cc01a25dd552e15a58cda64a5e8b4cb473ab8585e987776cf3c8279bccbb347cca4a30377689c39a011bbe38595d1a31152c19328'
