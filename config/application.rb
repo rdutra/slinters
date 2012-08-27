8df119cae9a2803bae482259b433a08b4c60a3dd
rdutra <rdutra@timbasoftware.com>
2012-08-27 15:34:09 -0300

    initialized devise

--- config/application.rb
+++ config/application.rb
@@ -52,6 +52,8 @@ module Slinters
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    #added for heroku
+    config.assets.initialize_on_precompile = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
