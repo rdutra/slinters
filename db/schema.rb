d03b0aa9736b3519f69815c8d4ff26dd6d9ac653
rdutra <rdutra@timbasoftware.com>
2012-09-04 17:47:03 -0300

    added register views

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120828153717) do
+ActiveRecord::Schema.define(:version => 20120831180453) do
+
+  create_table "expenses", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "incomes", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "services", :force => true do |t|
     t.integer  "user_id"
@@ -38,14 +48,6 @@ ActiveRecord::Schema.define(:version => 20120828153717) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.string   "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email"
-    t.integer  "failed_attempts",        :default => 0
-    t.string   "unlock_token"
-    t.datetime "locked_at"
-    t.string   "authentication_token"
     t.string   "fullname"
     t.string   "password"
   end
