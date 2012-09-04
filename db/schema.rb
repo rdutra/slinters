0e98f0144da08c8a9c2b6cf3ea36dc9b3f4d5b85
jgonzalez <jgonzalez@timbasoftware.com>
2012-09-04 17:50:45 -0300

    incomes and expenses

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120828153717) do
+ActiveRecord::Schema.define(:version => 20120831180453) do
+
+  create_table "expenses", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "incomes", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "services", :force => true do |t|
     t.integer  "user_id"
