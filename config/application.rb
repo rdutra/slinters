95eb956fc5383d0f68b1c64cb12b60606f18bd45
rdutra <rdutra@timbasoftware.com>
2012-09-06 16:46:13 -0300

    added mailer config

--- config/application.rb
+++ config/application.rb
@@ -55,6 +55,19 @@ module Slinters
     #added for heroku
     config.assets.initialize_on_precompile = false
 
+    #configure mailer
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.perform_deliveries = true
+    config.action_mailer.smtp_settings = {
+      :address         => "smtp.gmail.com",
+      :port            => 587,
+      :user_name       => "slinters.info@gmail.com",
+      :password        => "salesforce1",
+      :authentication  => "login",
+      :domain          => "slinters.info@gmail.com",
+      :enable_starttls_auto => true
+    }
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
